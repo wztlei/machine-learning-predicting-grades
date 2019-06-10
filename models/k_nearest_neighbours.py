00da2e05c71567e939ad5efad7d07d6391278cf9
WZTLEI <w.lei.thunderbolt@gmail.com>
2019-06-09 23:05:45 -0400

    Add different distance functions for k-nearest neighbours and add function documentation and typing

--- models/k_nearest_neighbours.py
+++ models/k_nearest_neighbours.py
@@ -1,57 +1,146 @@
 import csv
-import random
 import math
-import numpy as np
 import matplotlib.pylab as plt
+from typing import List, Dict, Callable
 
-def load_dataset(filename, print_file=False):
+
+def load_dataset(filename: str, print_file: bool = False) -> List[List[str]]:
+    """Reads the csv file containing the data for this project.
+
+    Args:
+        filename: A string denoting the name of the file storing the data.
+        print_file: True if the file is to be printed, and False otherwise.
+
+    Returns:
+        A list of all of the lines cf text contained within the data file.
+    """
+
     # Use a with/as block, which abstracts away opening and closing a file
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         lines = list(csv_reader)
-   
+
+        # Print the file contents if necessary
         if print_file:
             print('len(lines) =', len(lines))
 
             for line in lines:
                 print(line)
 
+        # Return the file contents as a 2D list of strings
         return lines
 
 
-def euclidean_distance(point_1, point_2):
+def minkowski_distance(point_1: List[float], point_2: List[float],
+                       p: int) -> float:
+    """Calculates the Minkowski distance between two points.
+
+    From Wikipedia: The Minkowski distance of order p between two points
+    X = (x1, ... , xn) and Y = (y1, ... , yn) is defined as
+    D(X, Y) = (sum_from_1_to_n( |xi - yi|^p )) ^ (1/p).
+
+        Args:
+            point_1: The coordinates for the 1st point as a list.
+            point_2: The coordinates for the 2nd point as a list.
+            p: The p-value for the Minkowski distance function.
+
+        Returns:
+            The Minkowski distance between point_1 and point_2.
+        """
+
     sum_of_squares = 0
 
     for (val_1, val_2) in zip(point_1, point_2):
-        sum_of_squares += math.pow(val_1 - val_2, 2)
+        sum_of_squares += math.pow(math.fabs(val_1 - val_2), p)
 
-    return math.sqrt(sum_of_squares)
-
-
-def swap(a, b):
-    a, b = b, a
+    return math.pow(sum_of_squares, 1/p)
 
 
-def insert_nearest_neighbours(nearest_neighbours, k, new_neighbour):
-    if len(nearest_neighbours) < k:
-        nearest_neighbours.append(new_neighbour)
-        return
+def manhattan_distance(point_1: List[float], point_2: List[float]) -> float:
+    """Calculates the Manhattan distance between two points.
 
-    for i in range(k):
+    Args:
+        point_1: The coordinates for the 1st point as a list.
+        point_2: The coordinates for the 2nd point as a list.
+
+    Returns:
+        The Manhattan distance between point_1 and point_2.
+    """
+    return minkowski_distance(point_1, point_2, 1)
+
+
+def euclidean_distance(point_1: List[float], point_2: List[float]) -> float:
+    """Calculates the Euclidean distance between two points.
+
+    Args:
+        point_1: The coordinates for the 1st point as a list.
+        point_2: The coordinates for the 2nd point as a list.
+
+    Returns:
+        The Euclidean distance between point_1 and point_2.
+    """
+    return minkowski_distance(point_1, point_2, 2)
+
+
+def insert_nearest_neighbours(nearest_neighbours: List[Dict],
+                              k: int, new_neighbour: Dict) -> None:
+    """Updates the list of nearest neighbours by inserting a new point
+
+    If nearest_neighbours has less than k items, then new_neighbour is inserted
+    into nearest_neighbours. Otherwise, new_neighbour is inserted into the list
+    and the item in nearest_neighbours with the greatest distance is removed.
+    nearest_neighbours always has the property that it is sorted with the
+    items increasing in distance from the data point currently examined.
+
+    Args:
+        nearest_neighbours: The pre-existing sorted list of neighbours that are
+            closest to the data point currently being classified.
+        k: The maximum size of nearest_neighbours.
+        new_neighbour: The new neighbouring data point to insert.
+    """
+    is_new_neighbour_inserted = False
+
+    # Iterate through all possible positions to insert in nearest_neighbours
+    for i in range(len(nearest_neighbours)):
+        # Insert in the appropriate place to preserve the sorted property
         if new_neighbour['distance'] < nearest_neighbours[i]['distance']:
-            nearest_neighbours[i], new_neighbour = new_neighbour, nearest_neighbours[i]
+            nearest_neighbours.insert(i, new_neighbour)
+            is_new_neighbour_inserted = True
+            break
+
+    # Append to the end of the list if necessary
+    if len(nearest_neighbours) < k and not is_new_neighbour_inserted:
+        nearest_neighbours.append(new_neighbour)
+    # Remove the item in the list with the largest distance if necessary
+    elif len(nearest_neighbours) > k:
+        nearest_neighbours.pop()
 
 
-def k_nearest_neighbours_model(students, k):
+def k_nearest_neighbours_from_scratch (
+        students: List[Dict], k: int,
+        distance_function: Callable = euclidean_distance) -> Dict:
+    """Predicts students' final grades with the k-nearest neighbours algorithm.
+
+    Args:
+        students: A list of dicts storing the data for each student,
+            including assessment marks and final grades.
+        k: The number of nearest neighbours to examine in the algorithm.
+        distance_function: The distance function to use for the algorithm.
+            Defaults to the Euclidean distance.
+    Returns:
+        A dict storing the results of the predictive algorithm.
+    """
     num_correct = 0
     sum_squared_errors = 0
 
+    # Iterate through every student to classify each one
     for student in students:
         other_students = [s for s in students if s != student]
         nearest_neighbours = []
 
+        # Iterate through the other student to identify the k-nearest neighbours
         for other_student in other_students:
-            distance = euclidean_distance(
+            distance = distance_function(
                 student['semester_grades'], other_student['semester_grades'])
 
             insert_nearest_neighbours(nearest_neighbours, k, {
@@ -59,48 +148,56 @@ def k_nearest_neighbours_model(students, k):
                 'student': other_student
             })
 
-        votes = {final_grade:0 for final_grade in range(1, 8)}
+        votes = {final_grade: 0 for final_grade in range(1, 8)}
 
+        # Determine the frequency of each category (ie. final grade)
+        # among the k-nearest neighbours
         for nn in nearest_neighbours:
             votes[nn['student']['final_grade']] += 1
 
-        # print(nearest_neighbours)
+        prediction, most_votes = None, None
 
-        model_prediction, most_votes = None, None
-
+        # Determine which category received the most votes among the neighbours
         for final_grade in votes:
             if most_votes is None or most_votes < votes[final_grade]:
-                model_prediction = final_grade
+                prediction = final_grade
                 most_votes = votes[final_grade]
 
-        # print('model_predicted=', most_common_key, ' actual=', student['final_grade'], ' id=', nearest_neighbours[0]['student']['id'])
-        if model_prediction == student['final_grade']:
+        # Determine if the student was categorized correctly
+        if prediction == student['final_grade']:
             num_correct += 1
 
-        sum_squared_errors += math.pow(model_prediction-student['final_grade'], 2)
+        # Update the sum of squared errors
+        sum_squared_errors += math.pow(prediction - student['final_grade'], 2)
 
+    # Calculate the results of the model
     num_students = len(students)
     percent_correct = num_correct / num_students * 100
-    percent_error = 100 - percent_correct
+    hamming_loss = (num_students - percent_correct)/ num_students
     mean_squared_error = sum_squared_errors / num_students
 
-    print("percent_correct=", "%.2f" % percent_correct, ", mse=", "%.2f" % mean_squared_error, sep="")
+    # Print the results
+    print("percent_correct=", "%.2f" % percent_correct, ", mse=",
+          "%.2f" % mean_squared_error, sep="")
+
+    # Return the results
     return {
         "percent_correct": percent_correct,
-        "percent_error": percent_error,
+        "hamming_loss": hamming_loss,
         "mean_squared_error": mean_squared_error
     }
 
 
 def main():
+    # Retrieve the raw data from the csv file
     raw_data = load_dataset('model_input_data.csv', print_file=False)
-    # print(raw_data)
     raw_data_without_headers = raw_data[1:]
 
+    # Organize the data into a list of dicts which each represent a student
     students = [{
         'id': student[0],
         'year': student[1],
-        'semester_grades': [float(grade) for grade in student[2:-2]],
+        'semester_grades': [float(grade) for grade in student[2:-5]],
         'predicted_grade': int(student[-2]),
         'final_grade': int(student[-1])
     } for student in raw_data_without_headers]
@@ -110,13 +207,12 @@ def main():
     for k in range(1, 31):
         print("for k=", k, ": ", sep="", end=" " if k < 10 else ""),
 
-        results = k_nearest_neighbours_model(students, k)
+        results = k_nearest_neighbours_from_scratch(students, k)
         k_list.append(k)
         percent_correct_list.append(results["percent_correct"])
         mean_squared_error_list.append(results["mean_squared_error"])
 
     # plt.subplot(nrows, ncols, index, **kwargs)
-
 
     plt.figure(figsize=(7, 8))
     plt.subplots_adjust(hspace=1)
